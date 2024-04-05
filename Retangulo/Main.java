b3685e36701d295acb7a4b643c828c1309f19896
Pedro Henrique Trajano da Silva <134293745+PedroHenriqueTDS@users.noreply.github.com>
2024-04-04 23:58:11 -0300

    Update Main.java

--- Retangulo/Main.java
+++ Retangulo/Main.java
@@ -1,5 +1,11 @@
-package Retangulo;
+packpage Retangulo;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        // cria um objeto Retangulo
+        Retangulo retangulo = new Retangulo(5.0, 3.0);
+
+        // Imprimindo os valores
+        retangulo.imprimir();
+    }
 }
